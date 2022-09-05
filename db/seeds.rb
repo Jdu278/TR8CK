b2c48c6e488a172f61ad032f967325b7eeb98f17
Maximilian Baer <jem04@web.de>
2022-09-05 12:57:51 +0200

    we can now to charts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 require 'faker'
+test = Faker::Date.unique.in_date_period(year: 2018, month: 2)
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
