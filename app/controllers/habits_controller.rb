566cd722c4da69493268ede5bf14e8e46d8e956a
Patryk Pysiak <patrykpysiak@gmail.com>
2022-09-06 09:08:52 +0200

    this is so much stuff...

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -2,31 +2,57 @@ class HabitsController < ApplicationController
   def index
     @habits=Habit.all
 
-  if params[:creationquery].present?
-    @habits = Habit.where("title ILIKE ?", "#{params[:creationquery]}")
-    if @habits.length == 0
-      respond_to do |format|
-        format.html
-        format.text { render partial: "list", locals: {habits: @habits}, formats: [:html] }
-      end
+    if  params[:creationquery].present? && Habit.where("title ILIKE ?", "#{params[:creationquery]}").blank?
+      partial = "chat"
+
+    elsif params[:creationquery].present? && Habit.where("title ILIKE ?", "#{params[:creationquery]}").present?
+      partial = "show"
+    else
+      partial = "list"
     end
-    return
 
 
-  elsif params[:query].present? && params[:creationquery].blank?
+
+    if params[:query].present?
     @habits = Habit.where("title ILIKE ?", "%#{params[:query]}%")
-  end
+    end
     respond_to do |format|
       format.html # Follow regular flow of Rails
-      format.text { render partial: "list", locals: {habits: @habits}, formats: [:html] }
+      format.text { render partial: partial, locals: {habits: @habits}, formats: [:html] }
     end
-  end
+    #  elsif params[:creationquery].present?{
+    #    @habits = Habit.where("title ILIKE ?", "#{params[:query]}")
+
+    #    respond_to do |format|
+    #      format.html
+    #      format.text { render partial: "chat",locals: {habits: @habits}, formats: [:html] }
+    #    end}
+
+    if params[:title].present?
+      @habit = Habit.new(habit_params)
+      params[:habit] = @habit
 
 
-  def show
-    @habit = Habit.find(params[:id])
+      if @habit.save
+        partial = "list"
+      else
+        flash[:alert] = "something went wrong with the creation process"
+        partial = "list"
+      end
+    end
+
+
   end
+
 
   def create
+
   end
+
+  private
+
+  def habit_params
+    params.permit(:title, :description, :needed_session_properties)
+  end
+
 end
--- app/javascript/controllers/search_habits_controller.js
+++ app/javascript/controllers/search_habits_controller.js
@@ -2,92 +2,191 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search-habits"
 export default class extends Controller {
-  static targets = ["form", "input", "habits", "chat", "chatinput"]
+  static targets = ["form", "input", "habits", "chatwindow", "chatinput", "submitit"]
 
   connect() {
-    console.log(this.formTarget)
-    console.log(this.inputTarget)
-    console.log(this.habitsTarget)
-  //   this.inputTarget.addEventListener("keyup", ({key}) => {
+    // console.log(this.formTarget)
+    // console.log(this.inputTarget)
+    // console.log(this.habitsTarget)
+    // console.log(this.submititTarget)
 
-  //     // if (Event.isComposing || key === "Enter") {
-  //     if (key === "Enter") {
 
-  //       this.chatTarget.classList.remove("invisible")
-  //       this.habitsTarget.classList.add("invisible")
-  //       this.formTarget.classList.add("invisible")
-  //       this.inputTarget.removeEventListener()
-  //       this.formTarget.preventDefault()
-  //     }
-  //     else
-  //       {const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
-  //       fetch(url, {headers: {"Accept": "text/plain"}})
-  //       .then(response => response.text())
-  //       .then((data) => {
-  //         console.log(data)
-  //         this.habitsTarget.outerHTML = data
-  //         // this.listTarget.outerHTML = data
-  //       })}
-  // })
+    var create = false
+    var newtitle = ""
+    var description = false
+    var newdescription = ""
+    var descriptiondone = false
+    var notesdone = false
+    var newnotes = false
+    var repetitionsdone = false
+    var newrepetitions = false
+    var mooddone = false
+    var newmood = false
 
-    this.inputTarget.addEventListener("submit", ({key}) => {
 
-        // if (Event.isComposing || key === "å") {
-      // if (key === "å") {
+        this.submititTarget.addEventListener("click", (event) => {
+          event.preventDefault()
+          if (this.inputTarget.value !== "yes" && create === false) newtitle = this.inputTarget.value
 
-        const url = `${this.formTarget.action}?creationquery=${this.inputTarget.value}`
-        fetch(url, {headers: {"Accept": "text/plain"}})
-        .then(response => response.text())
-        .then((data) => {
-          console.log({data})
-          this.habitsTarget.outerHTML = data
+          const url = `${this.formTarget.action}?creationquery=${this.inputTarget.value}`
+          fetch(url, {headers: {"Accept": "text/plain"}})
+          .then(response => response.text())
+          .then((data) => {
+            var chatw = document.getElementsByClassName("chat")
+            if (chatw.length === 0 ){
+            this.habitsTarget.outerHTML = data
+
+          }
+
+          this.inputTarget.value = ""
+          })
+
+
+          var chatw = document.getElementsByClassName("chat")
+
+          var messages = document.getElementsByClassName("message")
+
+
+          if (chatw.length === 1 && this.inputTarget.value === "yes" && create === false){
+
+              create = true
+
+              messages[0].insertAdjacentHTML("beforebegin", `<p class="userhere message">Yes.`)
+              messages[0].insertAdjacentHTML("beforebegin", `<p class="bothere message">Great! "${newtitle}" has been created! Would you like to add a description?</p>`)
+            }
+          else if (chatw.length === 1 && this.inputTarget.value === "yes" && create === true && description === false && descriptiondone === false) {
+
+
+
+            messages[0].insertAdjacentHTML("beforebegin", `<p class="userhere message">Yes.`)
+            messages[0].insertAdjacentHTML("beforebegin", `<p class="bothere message">Please type in the description below.</p>`)
+
+            description = true
+
+            }
+
+          else if (chatw.length === 1 && this.inputTarget.value === "no" && create === true && description === false && descriptiondone === false) {
+
+            messages[0].insertAdjacentHTML("beforebegin", `<p class="userhere message">No.`)
+            messages[0].insertAdjacentHTML("beforebegin", `<p class="bothere message">Roger that! No description for "${newtitle}" habit. Would you like to add notes in the future?</p>`)
+
+            descriptiondone = true
+            }
+
+          else if (chatw.length === 1 && description === true && descriptiondone === false) {
+
+            messages[0].insertAdjacentHTML("beforebegin", `<p class="userhere message">${this.inputTarget.value}`)
+            messages[0].insertAdjacentHTML("beforebegin", `<p class="bothere message">Description for "${newtitle}" is: "${this.inputTarget.value}". Would you like to add notes to the habit?</p>`)
+
+            newdescription = this.inputTarget.value
+            descriptiondone = true
+            }
+
+            else if (chatw.length === 1 && notesdone === false && descriptiondone === true && this.inputTarget.value === "no") {
+
+              messages[0].insertAdjacentHTML("beforebegin", `<p class="userhere message">${this.inputTarget.value}`)
+              messages[0].insertAdjacentHTML("beforebegin", `<p class="bothere message">Roger that! No notes for "${newtitle}" habit. Would you like to add repetitions to the habit?</p>`)
+
+              notesdone = true
+              }
+
+
+            else if (chatw.length === 1 && notesdone === false && descriptiondone === true && this.inputTarget.value === "yes") {
+
+              messages[0].insertAdjacentHTML("beforebegin", `<p class="userhere message">${this.inputTarget.value}`)
+              messages[0].insertAdjacentHTML("beforebegin", `<p class="bothere message">Notes option added to the "${newtitle}" habit. Would you like to add repetitions to the habit?</p>`)
+
+              newnotes = true
+              notesdone = true
+              }
+
+            else if (chatw.length === 1 && notesdone === true && repetitionsdone === false && this.inputTarget.value === "no") {
+
+              messages[0].insertAdjacentHTML("beforebegin", `<p class="userhere message">${this.inputTarget.value}`)
+              messages[0].insertAdjacentHTML("beforebegin", `<p class="bothere message">Roger that! No repetitions for "${newtitle}" habit. Would you like to add mood rating to the habit?</p>`)
+
+              repetitionsdone = true
+              }
+
+
+            else if (chatw.length === 1 && notesdone === true && repetitionsdone === false && this.inputTarget.value === "yes") {
+
+              messages[0].insertAdjacentHTML("beforebegin", `<p class="userhere message">${this.inputTarget.value}`)
+              messages[0].insertAdjacentHTML("beforebegin", `<p class="bothere message">Repetitions count added to the "${newtitle}" habit. Would you like to add mood rating to the habit?</p>`)
+
+              newrepetitions = true
+              repetitionsdone = true
+              }
+
+              else if (chatw.length === 1 && mooddone === false && repetitionsdone === true && this.inputTarget.value === "no") {
+
+                messages[0].insertAdjacentHTML("beforebegin", `<p class="userhere message">${this.inputTarget.value}`)
+                messages[0].insertAdjacentHTML("beforebegin", `<p class="bothere message">Roger that! No mood monitoring for "${newtitle}" habit. Almost done! Type "create" to save the habbit.</p>`)
+
+                mooddone = true
+                }
+
+
+              else if (chatw.length === 1 && mooddone === false && repetitionsdone === true && this.inputTarget.value === "yes") {
+
+                messages[0].insertAdjacentHTML("beforebegin", `<p class="userhere message">${this.inputTarget.value}`)
+                messages[0].insertAdjacentHTML("beforebegin", `<p class="bothere message">Mood monitoring added to the "${newtitle}" habit. Almost done! Type "create" to save the habbit.</p>`)
+
+                newmood = true
+                mooddone = true
+                }
+
+              else if (chatw.length === 1 && mooddone === true && this.inputTarget.value === "create") {
+
+                let newneeded_session_properties=[]
+                if (newrepetitions === true) newneeded_session_properties.push("repetitions")
+                if (newmood === true) newneeded_session_properties.push("mood")
+                if (newnotes === true) newneeded_session_properties.push("note")
+                const url = `${this.formTarget.action}?title=${newtitle}&description=${newdescription}&needed_session_properties=${newneeded_session_properties}`
+                fetch(url, {headers: {"Accept": "text/plain"}})
+                .then(response => response.text())
+                .then((data) => {
+                this.chatwindowTarget.outerHTML = data
+
+                })
+                }
+
+          else if (chatw.length === 1 && this.inputTarget.value !== ""){
+            console.log("oh snap. I don't know this command! D: but here is the status:")
+            console.log(`this fields input was: ${this.inputTarget.value}`)
+            console.log(`the proposed title for new habbit is: ${newtitle}`)
+            console.log(`chat visible? (yes if "1"): ${chatw.length}`)
+            console.log(`did you choose to create a new habit?: ${create}`)
+            console.log(`did you go through the decision to create a description?: ${descriptiondone}`)
+            console.log(`did you choose to add a description to your habbit?: ${description}`)
+            console.log(document.getElementsByClassName("bothere").innerHTML)
+
+            messages[0].insertAdjacentHTML("beforebegin", `<p class="userhere message"> ${this.inputTarget.value}!</p>`)
+            if (create === false) messages[0].insertAdjacentHTML("beforebegin", `<p class="bothere message">sorry, I didn't get that. Would you like to create a new habit named "${newtitle}"?</p>`)
+            else messages[0].insertAdjacentHTML("beforebegin", `<p class="bothere message">You seem tired. Perhaps take a nap and get back to this later?</p>`)
+            }
+
+
+
+          })
+
+
+        this.inputTarget.addEventListener("keyup", ({key}) => {
+          var chatw = document.getElementsByClassName("chat")
+          console.log(chatw.length)
+          if (chatw.length === 0)
+          {const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
+          fetch(url, {headers: {"Accept": "text/plain"}})
+          .then(response => response.text())
+          .then((data) => {
+            console.log("keyup")
+            this.habitsTarget.outerHTML = data
+
+          })}
+
         })
-      // }
-     }
-    )
-
-    this.inputTarget.addEventListener("keyup", ({key}) => {
-
-        {const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
-        fetch(url, {headers: {"Accept": "text/plain"}})
-        .then(response => response.text())
-        .then((data) => {
-          console.log(data)
-          this.habitsTarget.outerHTML = data
-          // this.listTarget.outerHTML = data
-        })}
-
-    })
   }
 
 
-  // ?new_habit=blank
 
-
-
-
-
-  update() {
-
-    if (this.formTarget.Event.key != "Enter")
-      {const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
-      fetch(url, {headers: {"Accept": "text/plain"}})
-      .then(response => response.text())
-      .then((data) => {
-      console.log(data)
-      this.habitsTarget.outerHTML = data
-        // this.listTarget.outerHTML = data
-      })}
-    else
-      {
-        this.chatTarget.classList.remove("invisible")
-      }
-    }
-
-  // submitit() {
-  //   this.inputTarget.addEventListener("keydown")
-  //   if (this.inputTarget.event.key != "Enter"){
-  //         this.chatTarget.classList.remove("invisible")
-  //       }
-  // }
 }
