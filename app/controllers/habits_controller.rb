922937708d9fa6ab8aba1995417b032b78d327bf
Tim Schauder <temscha@live.com>
2022-09-06 17:33:22 +0200

    added the graphs with proper function

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -14,7 +14,20 @@ class HabitsController < ApplicationController
 
   def show
     @habit = Habit.find(params[:id])
-    # @habit_sessions = @habit.habit_sessions
+
+    @habit_session = @habit.habit_sessions.where("created_at >?", Date.today - 14).group_by { |session| Date.parse session.created_at.to_s }
+    @habit_session_average = @habit_session.transform_values do |value|
+      variable = value.pluck(:mood)
+      variable.sum / variable.size.to_f
+    end
+    @habit_session_d_average = @habit_session.transform_values do |value|
+      variable = value.pluck(:duration)
+      variable.sum / variable.size.to_f
+    end
+    @habit_session_r_average = @habit_session.transform_values do |value|
+      variable = value.pluck(:repetitions)
+      # variable.sum / variable.size.to_f
+    end
   end
 
   def create
